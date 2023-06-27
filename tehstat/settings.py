2e4bdc182d69c2b5079003fa3deafbe7050b78f5
Jetstream <spektres@mail.ru>
2023-06-27 09:26:20 +0300

    sfgs

--- tehstat/settings.py
+++ tehstat/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tehstat',
-        'USER': 'postgres',
+        'USER': 'tehstat',
         'PASSWORD': '2536',
         'HOST': 'localhost',
         'PORT': '5432',
