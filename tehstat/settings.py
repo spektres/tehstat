a3e2a5febd461b37c986e42a8e5a8799ff1e10ff
Jetstream <spektres@mail.ru>
2023-06-27 09:23:32 +0300

    bd

--- tehstat/settings.py
+++ tehstat/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'tehstat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tehstat',
+        'USER': 'postgres',
+        'PASSWORD': '2536',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
